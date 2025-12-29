e32da28bbdccc0a02e14d28e41fe07912e03d0ed
deividfoggi <deividfoggi@hotmail.com>
2025-12-29 18:46:10 -0300

    Configure Azure provider to use Entra ID auth for storage accounts
    Added storage_use_azuread = true to provider configuration to support
    storage accounts with shared_access_key_enabled = false

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,6 +25,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  
+  # Configure storage account data plane to use Entra ID authentication
+  # Required when storage accounts have shared_access_key_enabled = false
+  storage_use_azuread = true
 }
 
 data "azurerm_client_config" "current" {}
